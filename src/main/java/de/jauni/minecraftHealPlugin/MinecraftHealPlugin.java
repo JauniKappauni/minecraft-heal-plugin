16977c5f9aeb5ace01c694d03637daef3a7ee91b
JauniKappauni <social.jauni@gmail.com>
2025-03-10 21:41:17 +0100

    Initial Commit

---
+++ src/main/java/de/jauni/minecraftHealPlugin/MinecraftHealPlugin.java
@@ -0,0 +1,17 @@
+package de.jauni.minecraftHealPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftHealPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
